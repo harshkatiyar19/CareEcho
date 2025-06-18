1f4f8b62be1fce88376fb2d11ef76c227e130e5a
harshkatiyar19 <harshkatiyar03.w@gmail.com>
2025-06-18 19:56:27 +0530

    Initial commit

---
+++ src/main/java/com/example/CareEcho/CareEchoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CareEcho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareEchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareEchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CareEcho/CareEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CareEcho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareEchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
